471d6b6f74318a95314438d9f223494d283fbafa
Jason Liu <liu.jason84@gmail.com>
2020-05-18 19:35:11 -0400

    Created byteprod function that finds the product between two byte arrays

--- main.c
+++ main.c
@@ -4,13 +4,13 @@ void montprod(byte* dest, byte* a, byte* b);
 void byteprod(byte* dest, byte* a, byte* b, int size_a, int size_b);
 int main(void)
 {
-	byte a[] = {5};
-	byte b[] = {6};
+	byte a[] = {1,5};
+	byte b[] = {0,6};
 	byte* results = malloc(sizeof(byte)*2);
 	memset(results, 0, sizeof(byte)*2);
 
 	printf("testing byteproduct\n");
-	byteprod(&results, &a, &b, 8, 8);
+	byteprod(results, &a, &b, sizeof(a), sizeof(b));
 	printf("results are %d,%d\n",results[0],results[1]);
 }
 
@@ -29,16 +29,9 @@ void montprod(byte* dest, byte* a, byte* b)
 
 void byteprod(byte* dest, byte* a, byte* b, int size_a, int size_b){
 	//ensure when using byteprod the destination buffer is at least twice the size of the input buffers
-	int or_flag = 1;
-	for(int i = 0 ; i < size_a; i++){
-		if(*a & (1<<i)){
-			if(or_flag){
-				*dest |= *b;
-				or_flag = 1;
-			}
-			else{
-				*dest &= (*b << i);
-			}
+	for(int i = 0; i< size_a; i++){
+		for(int j = 0; j < size_b; j++){
+			*((short*)(dest+i)) += a[i]*b[j];
 		}
 	}
 }
